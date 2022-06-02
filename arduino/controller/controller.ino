4b1a797d90705a4c0de789aa6397efb903865e3d
Lauri Laux <lauripaulo@gmail.com>
2022-06-02 18:55:19 -0300

    Initial primitive state machine added.

--- arduino/controller/controller.ino
+++ arduino/controller/controller.ino
@@ -1,52 +1,55 @@
 #include <Wire.h>
-
-// Arnakazim's TwiLiquidCrystal Arduino Library
-// https://github.com/arnakazim/TwiLiquidCrystal-library
-#include <TwiLiquidCrystal.h>
+#include <TwiLiquidCrystal.h>                     // Arnakazim's TwiLiquidCrystal Arduino Library
 
 // Create an LCD object and setting the I2C address
 #define LCD_ADDRESS 0x3f
-TwiLiquidCrystal lcd(LCD_ADDRESS);
 
-String isEspResponseComplete = "";  // a String to hold incoming data
-bool stringComplete = false;        // whether the string is complete
+#define STATE_IDLE              1
+#define STATE_ERROR             2
+#define STATE_PROBING_ESP8266   3
+
+const String ESP_TEST      = "AT\n";                // This will check if the module is connected properly and its functioning, the module will reply with an acknowledgment.
+const String ESP_RESET     = "AT+RST";              // This will reset the wifi module. Its good practice to reset it before or after it has been programmed.
+const String ESP_INFO      = "AT+GMR";              // This will mention the firmware version installed on the ESP8266.
+const String ESP_LIST      = "AT+CWLAP";            // This will detect the Access points and their signal strengths available in the area.
+const String ESP_CONNECT   = "AT+CWJAP=";           // AT+CWJAP=”SSID”,”PASSWORD” This connects the ESP8266 to the specified SSID in the AT command mentioned in the previous code.
+const String ESP_IP        = "AT+CIFSR";            // This will display the ESP8266’s obtained IP address.
+const String ESP_DISCONECT = "AT+CWJAP=\"\",\"\"";  // If the user wants to disconnect from any access point.
+const String ESP_SETMODE   = "AT+CWMODE=1";         // This sets the Wifi mode. It should be always set to Mode 1 if the module is going to be used as a node
+
+const TwiLiquidCrystal lcd(LCD_ADDRESS);
+
+// String uartContent = "";         // a String to hold incoming data
+bool globalUartReadComplete = false;    // whether the string is complete
 bool isLcdEnabled = false;          // Enable LCD
-int initEspStep = 0;
+byte globalState = STATE_IDLE;
+String globalUartBuffer = "";
 
+String getUartBuffer() {
+  return globalUartBuffer;
+}
+
+void setUartBuffer(String buffer) {
+  globalUartBuffer = buffer;
+}
+
+void changeGlobalState(byte state) {
+  globalState = state;
+}
+
+byte getGlobalState() {
+  return globalState;
+}
 
 void setup() {
   Wire.begin();
   Serial.begin(115200);
   Serial3.begin(115200);
   while (!Serial && !Serial3)
-     ; // waiting for the serial monitor to become available
+    ;  // waiting for the serial monitor to become available
   // All set, we can proceed.
-  Serial.println("\nBeer controller starting");
-  isEspResponseComplete.reserve(128);
-  initLcd();
-}
-
-void executeEspSteps() {
-  switch (initEspStep) {
-    case 0:
-      Serial.println("Step 1");
-      Serial3.write("AT\n");
-      initEspStep ++;
-      delay(500);
-      break;
-    case 1:
-      Serial.println("Step 2");
-      Serial3.write("AT+GMR\n");
-      initEspStep ++;
-      delay(500);
-      break;
-    case 2:
-      // nothing else to do.
-      break;
-    default:
-      Serial.println("Not a valid step.");
-  }
-  Serial3.flush();
+  changeGlobalState(STATE_PROBING_ESP8266);
+  Serial.println("Beer controller starting");
 }
 
 void initLcd() {
@@ -58,43 +61,84 @@ void initLcd() {
 }
 
 /*
-  This routine is run between each time loop() runs, so using delay inside 
+  This routine is run between each time loop() runs, so using delay inside
   loop can delay response. Multiple bytes of data may be available.
 */
-void evaluateESP() {
+void readUART() {
   while (Serial3.available()) {
     // get the new byte:
     char inChar = (char)Serial3.read();
-    // add it to the isEspResponseComplete:
-    isEspResponseComplete += inChar;
+    // add it to the uartContent:
+    setUartBuffer(getUartBuffer() + inChar);
     // if the incoming character is a newline, set a flag so the main loop can
     // do something about it:
     if (inChar == '\n') {
-      stringComplete = true;
+      setUartReadComplete(true);
     }
   }
 }
 
-void loop() {
-  // print the string when a newline arrives:
-  executeEspSteps();
-  evaluateESP();
-  if (stringComplete) {
-    Serial.println(isEspResponseComplete);
-    // clear the string:
-    isEspResponseComplete = "";
-    stringComplete = false;
+void resetUartReadState() {
+  setUartReadComplete(false);
+  setUartBuffer("");
+}
+
+bool isUartReadComplete() {
+  return globalUartReadComplete;
+}
+
+void setUartReadComplete(bool completed) {
+  globalUartReadComplete = completed;
+}
+
+bool isEsp8266Present() {
+  bool res = false;
+  Serial3.write("AT+GMR\n ");
+  delay(200);
+  readUART();
+  Serial.print("UART: ");
+  Serial.println(getUartBuffer());
+  if (getUartBuffer().lastIndexOf("OK") > 0) {
+    res = true;
   }
-// // Update and send command to other serial
-//  if (Serial.available()) {
-//    char resp = (char)Serial.read();
-//    Serial3.write(resp);
-//  }
+  return res;
 }
 
 /*
- * Helper function to find first i2c address in the bus.
- */
+  void loop() {
+  if (getGlobalState() == STATE_PROBING_ESP8266) {
+    resetUartReadState();
+    Serial.println("Testing ESP8266 conectivity...");
+    if (isEsp8266Present()) {
+      Serial.println("ESP8266 found at Serial3 (UART).");
+      changeGlobalState(STATE_IDLE);
+    } else {
+      Serial.println("Error, ESP8266 not found.");
+      Serial.println("Fatal ERROR.");
+      changeGlobalState(STATE_ERROR);
+    }
+  }
+  }
+*/
+
+void loop() {
+  // print the string when a newline arrives:
+  readUART();
+  if (isUartReadComplete()) {
+    Serial.print(getUartBuffer());
+    // clear the string:
+    resetUartReadState();
+  }
+  // Update and send command to other serial
+  if (Serial.available()) {
+    char resp = (char)Serial.read();
+    Serial3.write(resp);
+  }
+}
+
+/*
+   Helper function to find first i2c address in the bus.
+*/
 byte list_i2c_devices() {
   int nDevices = 0;
   Serial.println("Scanning...");
